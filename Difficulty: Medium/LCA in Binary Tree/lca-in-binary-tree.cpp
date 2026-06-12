04d28521c78d576d29ee90836ecd2ef276dd67cb
Raja Bhaiya Maurya <rajabhaiyamaurya390@gmail.com>
2026-06-12 13:42:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/LCA in Binary Tree/lca-in-binary-tree.cpp
@@ -0,0 +1,48 @@
+/*
+Definition for Node
+class Node {
+	public:
+	int data;
+	Node* left;
+	Node* right;
+	
+	Node(int val) {
+		data = val;
+		left = right = nullptr;
+	}
+};
+*/
+
+class Solution {
+	public:
+	Node* lca(Node* root, int n1, int n2) {
+		//  code here
+		if (root == NULL) {
+			return NULL;
+		}
+		if(root->data==n1||root->data==n2){
+		    return root;
+		}
+		
+		Node* left = lca(root->left, n1, n2);
+	
+		Node* right = lca(root->right, n1, n2);
+		
+			if (left == NULL && right == NULL) {
+			return NULL;
+		}
+		
+		if (left != NULL && right == NULL) {
+			return left;
+		}
+		if (left == NULL && right != NULL) {
+			return right;
+		}
+		
+		if (left != NULL && right != NULL) {
+			return root;
+		}
+		
+		
+	}
+};
